6aaaccaa18abe94fadbd608f1f39a2442e0133b7
Tom Christie <tom@tomchristie.com>
2019-04-19 09:33:11 +0100

    Add response.iter_content() and response.iter_lines()

--- requests_async/models.py
+++ requests_async/models.py
@@ -1,8 +1,20 @@
+import codecs
 from requests.models import PreparedRequest, Request, Response as BaseResponse
 from .exceptions import ContentNotAvailable
 
 
 ITER_CHUNK_SIZE = 512
+
+
+async def stream_decode_response_unicode(aiterator, encoding):
+    decoder = codecs.getincrementaldecoder(encoding)(errors='replace')
+    async for chunk in aiterator:
+        rv = decoder.decode(chunk)
+        if rv:
+            yield rv
+    rv = decoder.decode(b'', final=True)
+    if rv:
+        yield rv
 
 
 class Response(BaseResponse):
@@ -13,13 +25,13 @@ class Response(BaseResponse):
         return self._content
 
     async def iter_content(self, chunk_size=1, decode_unicode=False):
-        async def generate():
-            if self._content is False:
-                stream = self.raw.stream
-            else:
-                async def stream():
-                    yield self._content
+        if self._content is False:
+            stream = self.raw.stream
+        else:
+            async def stream():
+                yield self._content
 
+        async def generate():
             data = b''
             async for part in stream():
                 data += part
@@ -29,11 +41,38 @@ class Response(BaseResponse):
             if data:
                 yield data
 
-        pass
+        if decode_unicode and self.encoding is not None:
+            async for chunk in stream_decode_response_unicode(generate(), self.encoding):
+                yield chunk
+        else:
+            async for chunk in generate():
+                yield chunk
+
 
     async def iter_lines(self, chunk_size=ITER_CHUNK_SIZE, decode_unicode=False, delimiter=None):
-        pass
+        pending = None
+
+        async for chunk in self.iter_content(chunk_size=chunk_size, decode_unicode=decode_unicode):
+
+            if pending is not None:
+                chunk = pending + chunk
+
+            if delimiter:
+                lines = chunk.split(delimiter)
+            else:
+                lines = chunk.splitlines()
+
+            if lines and lines[-1] and chunk and lines[-1][-1] == chunk[-1]:
+                pending = lines.pop()
+            else:
+                pending = None
+
+            for line in lines:
+                yield line
+
+        if pending is not None:
+            yield pending
 
     async def __aiter__(self):
         """Allows you to use a response as an iterator."""
-        return await self.iter_content(128)
+        return self.iter_content(128)
--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -6,9 +6,90 @@ import requests_async
 
 
 @pytest.mark.asyncio
-async def test_get(server):
-    url = "http://127.0.0.1:8000/"
+async def test_content_not_available_on_stream(server):
+    url = "http://127.0.0.1:8000/hello_world"
     response = await requests_async.get(url, stream=True)
     assert response.status_code == 200
     with pytest.raises(requests_async.exceptions.ContentNotAvailable):
         response.content
+
+
+@pytest.mark.asyncio
+async def test_iter_content_on_stream(server):
+    url = "http://127.0.0.1:8000/hello_world"
+    response = await requests_async.get(url, stream=True)
+    assert response.status_code == 200
+    content = b''
+    async for chunk in response.iter_content():
+        assert len(chunk) == 1
+        content += chunk
+    assert content == b'Hello, world!'
+
+
+@pytest.mark.asyncio
+async def test_iter_text_on_stream(server):
+    url = "http://127.0.0.1:8000/hello_world"
+    response = await requests_async.get(url, stream=True)
+    assert response.status_code == 200
+    content = ''
+    async for chunk in response.iter_content(decode_unicode=True):
+        assert len(chunk) == 1
+        content += chunk
+    assert content == 'Hello, world!'
+
+
+@pytest.mark.asyncio
+async def test_iter_content_on_content(server):
+    url = "http://127.0.0.1:8000/hello_world"
+    response = await requests_async.get(url)
+    assert response.status_code == 200
+    content = b''
+    async for chunk in response.iter_content():
+        assert len(chunk) == 1
+        content += chunk
+    assert content == b'Hello, world!'
+
+
+@pytest.mark.asyncio
+async def test_iter_text_on_content(server):
+    url = "http://127.0.0.1:8000/hello_world"
+    response = await requests_async.get(url)
+    assert response.status_code == 200
+    content = ''
+    async for chunk in response.iter_content(decode_unicode=True):
+        assert len(chunk) == 1
+        content += chunk
+    assert content == 'Hello, world!'
+
+
+@pytest.mark.asyncio
+async def test_iter(server):
+    url = "http://127.0.0.1:8000/hello_world"
+    response = await requests_async.get(url, stream=True)
+    assert response.status_code == 200
+    content = b''
+    async for chunk in response:
+        content += chunk
+    assert content == b'Hello, world!'
+
+
+@pytest.mark.asyncio
+async def test_iter_lines(server):
+    url = "http://127.0.0.1:8000/hello_world"
+    response = await requests_async.get(url, stream=True)
+    assert response.status_code == 200
+    lines = []
+    async for line in response.iter_lines(decode_unicode=True):
+        lines.append(line)
+    assert lines == ['Hello, world!']
+
+
+@pytest.mark.asyncio
+async def test_iter_lines_with_delimiter(server):
+    url = "http://127.0.0.1:8000/hello_world"
+    response = await requests_async.get(url, stream=True)
+    assert response.status_code == 200
+    lines = []
+    async for line in response.iter_lines(decode_unicode=True, delimiter=' '):
+        lines.append(line)
+    assert lines == ['Hello,', 'world!']
